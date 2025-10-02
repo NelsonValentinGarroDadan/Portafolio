28647c0176e2993559eaf474e94ece0056eac044
Velentin Garro <nelsonvgarrodadan@gmail.com>
2025-10-02 11:31:40 -0300

    la aurora no me aviso

--- src/Componentes/Presentacion.jsx
+++ src/Componentes/Presentacion.jsx
@@ -10,15 +10,15 @@ function Presentacion (){
                     <img fetchpriority="high" src='./Imagenes/avatar_sinbg.webp'  alt="avtar-pinguino"/>
                 </div>
                 <div className="content">
-                    <h1 className="name">Valentin Garro</h1>
+                    <div className="name">Valentin Garro</div>
                     <div className="job">
                         <span className="text-gradient"> FullStack Developer </span>
                     </div>
                     <div className="des">
                         Full Stack Developer con fuerte enfoque en Backend, especializado en Node.js, NestJS, React y TypeScript.
-Me adapto rápidamente a nuevos entornos y proyectos, analizando problemas con claridad y proponiendo soluciones eficientes.
-Busco siempre un equilibrio entre un código sólido y entregas rápidas de producto, asegurando valor real para el equipo y los usuarios.
-Trabajo de manera colaborativa, aprendiendo de cada desafío sin perder seguridad en mis capacidades técnicas.
+                        Me adapto rápidamente a nuevos entornos y proyectos, analizando problemas con claridad y proponiendo soluciones eficientes.
+                        Busco siempre un equilibrio entre un código sólido y entregas rápidas de producto, asegurando valor real para el equipo y los usuarios.
+                        Trabajo de manera colaborativa, aprendiendo de cada desafío sin perder seguridad en mis capacidades técnicas.
                     </div>
                     <div className="btn-skills">
                         <a data-tab="Habilidades" href="Habilidades" className="text-gradient link-nav">
